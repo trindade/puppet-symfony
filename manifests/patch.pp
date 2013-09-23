cdd8c81963fcfa915aa55be903d80c38343e8023
Erika Heidi <erika.christina@gmail.com>
2013-09-23 16:36:49 +0200

    fixed patch checking

--- manifests/patch.pp
+++ manifests/patch.pp
@@ -17,6 +17,6 @@ define symfony::patch(
   exec { "AppKernel-patch":
     cwd     => "/vagrant/app",
     command => "/usr/bin/patch -ts </tmp/AppKernel.patch",
-    onlyif  => "grep -c 'getCacheDir' AppKernel.php"
+    unless  => "grep -c 'getCacheDir' AppKernel.php"
   }
 }
