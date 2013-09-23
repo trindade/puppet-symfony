aa495251417e0b033091b24404847a3e5beb5d4f
Erika Heidi <erika.christina@gmail.com>
2013-09-23 15:19:08 +0200

    Added optimization patch

---
+++ manifests/patch.pp
@@ -0,0 +1,21 @@
+############################################################################################
+# Patch for the AppKernel
+# Optimizes Symfony apps running with Vagrant by moving the cache and log dirs outside NFS
+# As stated here: http://www.whitewashing.de/2013/08/19/speedup_symfony2_on_vagrant_boxes.html
+############################################################################################
+
+define symfony::patch(
+) {
+
+  file {'patch-file':
+    ensure => present,
+    path   => '/tmp/AppKernel.patch',
+    mode   => '0644',
+    source => 'puppet:///modules/puppet-symfony/AppKernel.patch',
+  }
+
+  exec { "AppKernel-patch":
+    cwd     => "/vagrant/app",
+    command => "/usr/bin/patch -ts <AppKernel.patch",
+  }
+}
